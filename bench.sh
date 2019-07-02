e4c417d095d9954c92664e4ad13134e8b5c6aa19
Hans Petter Jansson <hpj@cl.no>
2019-07-02 23:05:37 +0200

    bench: Add benchmarking and plotting scripts

---
+++ bench.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+DESC_gdkpixbuf="GDK-Pixbuf"
+DESC_pixman="Pixman"
+DESC_sdl="SDL_gfx"
+DESC_skia="Skia"
+DESC_smolscale="Smolscale"
+DESC_smolscale_mt="Smolscale MT"
+
+TESTNAME_gdkpixbuf="gdk_pixbuf"
+TESTNAME_pixman="pixman"
+TESTNAME_sdl="sdl"
+TESTNAME_skia="skia"
+TESTNAME_smolscale="smol"
+TESTNAME_smolscale_mt="smol-mt"
+
+UNITS=" \
+gdkpixbuf
+pixman
+sdl
+skia
+smolscale
+smolscale_mt
+"
+
+TESTS=" \
+50-2000-2000-0.001-1.5-500
+10-16383-16383-0.0001-0.1-100
+"
+
+TESTS_AVAILABLE="\
+20-2000-2000-0.001-1.5-50
+50-2000-2000-0.001-1.5-500
+10-16383-16383-0.0001-0.1-100
+"
+
+LD_LIBRARY_PATH="skia/out/Shared"
+
+rm -Rf results
+mkdir -p results
+
+for UNIT in $UNITS; do
+  for TEST in $TESTS; do
+    echo $UNIT: $TEST
+
+    eval DESC=\${DESC_${UNIT}}
+    eval TESTNAME=\${TESTNAME_${UNIT}}
+
+    echo \"${DESC}\" >>results/resize-$TEST.txt
+#    # Quick
+#    ./test $TESTNAME proportional 20 $TEST $TEST 0.001 1.5 50 >>results/resize-$TEST.txt
+    # Slow
+#    ./test-simd $TESTNAME proportional 20 $TEST $TEST 0.001 1.5 500 >>results/resize-$TEST.txt
+    ./test-simd $TESTNAME proportional $(echo $TEST | sed 's/-/ /g') >>results/resize-$TEST.txt
+    echo >>results/resize-$TEST.txt
+    echo >>results/resize-$TEST.txt
+  done
+done
