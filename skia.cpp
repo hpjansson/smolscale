57230077a5a029e5d24c5c9f6722bc896ea25f68
Hans Petter Jansson <hpj@cl.no>
2019-07-05 01:07:46 +0200

    test: Add missing copyright note

--- skia.cpp
+++ skia.cpp
@@ -1,5 +1,7 @@
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
+/* Copyright © 2019 Hans Petter Jansson. See COPYING for details. */
+
 #include <stdexcept>
 #include <SkPixmap.h>
 
